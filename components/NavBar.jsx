a135163d8081ffc61a2ecf0f8eea99fab0430134
Alvaro R Martin <melkor@aleph.engineering>
2023-09-25 01:13:00 -0400

    #2 Finish the SideBar Configuration
    Implements las changes for the SideBar Behabior

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -9,6 +9,21 @@ const NavBar = () => {
   const { data: session } = useSession();
   const [providers, setProviders] = useState(null)
   const [toggleMenu, setToggleMenu] = useState(false)
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth > 650);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const SetProviders = async () => {
@@ -19,11 +34,11 @@ const NavBar = () => {
     SetProviders();
   }, [])
   return (
-    <nav className={`flex items-center flex-between w-full pt-2  ${session?.user ? 'pl-4' : 'pl-8'}  pr-8 m-2 mt-0 top-0 bg-transparent fixed z-10`}>
-      <Link href={session?.user ? '/dashboard' : '/'} className="flex gap-2 flex-center flex-col">
+    <nav className={`flex items-center  w-full pt-2  ${session?.user && isWideScreen? 'flex-end pl-4' : 'flex-between pl-8'}  pr-8 m-2 mt-0 top-0 bg-transparent h-[90px] fixed z-[15]`}>
+      {!session?.user || !isWideScreen ? <Link href={session?.user ? '/dashboard' : '/'} className="flex gap-2 flex-center flex-col">
         <Image src="/assets/logo.svg" alt="Scope-IT Logo" width={50} height={50} className="object-contain" />
         <p className="logo_text">Scope-IT</p>
-      </Link>
+      </Link> : <></>}
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
         {session?.user ? <div className="flex gap-3 md:gap-5">
@@ -45,8 +60,9 @@ const NavBar = () => {
           <img src={session?.user.image} alt="profile" width={37} height={37} className="rounded-full" onClick={() => setToggleMenu((prev) => !prev)} />
           {toggleMenu && (
             <div className="dropdown">
+              <Link href="/estimations" className="dropdown_link" onClick={() => setToggleMenu(false)}>New Estimation</Link>
+              <Link href="/features" className="dropdown_link" onClick={() => setToggleMenu(false)}>Features</Link>
               <Link href="/profile" className="dropdown_link" onClick={() => setToggleMenu(false)}>My Profile</Link>
-              <Link href="/new_estimation" className="dropdown_link" onClick={() => setToggleMenu(false)}>New Estimation</Link>
               <button type="button" className="mt-5 w-full black_btn" onClick={() => { setToggleMenu(false); signOut() }}>Sign Out</button>
             </div>
           )}
--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,18 +1,16 @@
 'use client'
 import { sideBarLinks } from "@constants/constants";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const SideBar = () => {
   const router = useRouter();
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState('none');
 
-  const toggleNAvMenu = () => {
-    if (isActive) {
-      setIsActive(false);
-    } else {
-      setIsActive(true);
-    }
+  const activateItem = (target) => {
+    console.log("Item Clicked: " + target);
+    setIsActive(target)
   };
 
   const navigate = (route) => {
@@ -20,12 +18,14 @@ const SideBar = () => {
   }
 
   return (
-    <div className="side-bar absolute left-0 flex flex-col justify-start pt-32 items-center pl-3 h-full nav-background">
-      <ul className="side-nav flex flex-col gap-6 pt-6" onClick={toggleNAvMenu}>
+    <div className="side-bar absolute left-0 flex flex-col justify-start  items-center pt-3 h-full nav-background z-20">
+      <div onClick={() => {navigate('/dashboard'); activateItem('none')}} className="flex gap-2 flex-center flex-col mb-6 cursor-pointer">
+        <Image src="/assets/logo.svg" alt="Scope-IT Logo" width={50} height={50} className="object-contain" />
+        <p className="logo_text">Scope-IT</p>
+      </div>
+      <ul className="side-nav flex flex-col gap-8 pt-6">
         {sideBarLinks.map((_link, index) => (
-          <li key={index} onClick={() => navigate(_link.link)} className={({ isActive }) =>
-            isActive ? "active cursor-pointer" : "cursor-pointer"
-          }><div className="flex items-center gap-1 side-link-item">
+          <li key={index} onClick={() => {navigate(_link.link); activateItem(_link.link)}} className={isActive == _link.link ? 'active' : ''}><div className={`flex items-center gap-1 side-link-item`}>
               <span className="icon">{_link.icon}</span><span className="side-nav-text">{_link.title}</span></div></li>
         ))}
       </ul>
