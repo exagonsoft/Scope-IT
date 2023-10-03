076498bafe18613d281f551b0be2949ba6afc453
Alvaro R Martin <melkor@aleph.engineering>
2023-10-03 02:41:14 -0400

    #3 Authentication Method
    Implement SignIn and SignUp pages and components

--- src/pages/auth/signinPage.jsx
+++ src/pages/auth/signinPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './authstyles.css'
+import SignInForm from '../../components/authComponents/signinForm'
 
 const SignInPage = () => {
   return (
-    <div>
-      Sign In
+    <div className='w-full h-screen flex justify-center items-center'>
+      <SignInForm />
     </div>
   )
 }
--- src/pages/auth/signupPage.jsx
+++ src/pages/auth/signupPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './authstyles.css'
+import SignUpForm from '../../components/authComponents/signupForm'
 
 const SignUpPage = () => {
   return (
-    <div>
-      
+    <div className='w-full h-screen flex justify-center items-center'>
+      <SignUpForm />
     </div>
   )
 }
